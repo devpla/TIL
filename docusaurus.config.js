b01a3bd1f228c6b9206af50e2486af04550ed87b
devpla <chaeyeonhee@kakao.com>
2021-12-16 00:38:47 +0900

    design: Update page logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: "Frontend",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
         items: [
           {
