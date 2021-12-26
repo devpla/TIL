4a829a574fd295e41172e2120344e196b7e6adea
devpla <chaeyeonhee@kakao.com>
2021-12-26 16:42:28 +0900

    wip: Update button url

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg hero__button"
-            to="/docs/intro"
+            to="/docs/frontend/intro"
           >
             Get Started
           </Link>
